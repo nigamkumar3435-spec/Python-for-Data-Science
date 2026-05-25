891dcf307b23468f5ce306c79e9f01815683bd8d
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-05-25 23:05:27 +0530

    Implement break statement examples in Breakstatement.py
    Add examples demonstrating the use of break statements in loops.

--- FlowControl&Loops/Breakstatement.py
+++ FlowControl&Loops/Breakstatement.py
@@ -1,1 +1,21 @@
+#Break Statement
 
+#find the first even number in a list
+numbers=[1,3,58,9,10]
+
+for num in numbers:
+  if num%2==0:
+    print("First even number found:",num)
+    break
+
+#Output: First even number found: 58
+
+#search for a specific element in a list
+target_element=42
+data=[10,20,30,42,50]
+
+for element in data:
+  if element == target_element:
+    print("Element found")
+    break
+#Output: Element found
