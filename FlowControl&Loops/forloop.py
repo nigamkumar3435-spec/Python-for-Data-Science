0f0162e23c731966611bb00b7a00b47f5994164e
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-05-25 23:03:16 +0530

    Add for loop examples in forloop.py
    Added examples of using for loops to iterate over a list and calculate a sum.

--- FlowControl&Loops/forloop.py
+++ FlowControl&Loops/forloop.py
@@ -1,1 +1,16 @@
+#For loop
 
+#iterate over a list of colors
+colors={"red","green","blue"}
+for color in colors:
+  print(color)
+#Output: blue
+#         red
+#       green
+
+#calculate the sum of numbers from 1 to 5 using a for loop
+sum_result=0
+
+for i in range(1,6):
+  sum_result+=i
+print("Sum:",sum_result) #Output: Sum: 15
