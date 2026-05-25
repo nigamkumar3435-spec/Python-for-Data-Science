3df49c5338768f70ba009fa089e63e5d6e7b1a46
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-05-25 22:58:16 +0530

    Add if-elif-else examples for number and voting checks

--- FlowControl&Loops/ifelifelse.py
+++ FlowControl&Loops/ifelifelse.py
@@ -1,1 +1,17 @@
+#if-elif-else
 
+#check if a number is positive,negative or zero
+num=-5
+if num>0:
+  print("Positive")
+elif num<0:
+  print("Negative")
+else:
+  print("Zero")  #Output: Negative
+
+#Check if a person is eligible to vote or not
+age=17
+if age>=18:
+  print("Eligible to vote")
+else:
+  print("Not Eligible to vote") #Output: Not Eligible to vote
